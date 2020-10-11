829cb6b36712ec4e510df2362b8bb4c21b69b8c8
Anuj Negi <anuj5negi5@gmail.com>
2020-10-11 23:17:49 +0530

    Team Component done

--- src/components/Team/ShowcaseTeam/ShowcaseTeam.js
+++ src/components/Team/ShowcaseTeam/ShowcaseTeam.js
@@ -7,10 +7,13 @@ const ShowcaseTeam = () => {
   return (
     <div className={classes.ShowcaseTeam}>
       <div className={classes.container}>
-        <h1 className={classes.teamHeader}>
-          Team
-        </h1>
-        <p className={classes.teamInfo}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, dicta est consequuntur hic atque ullam non quis nisi fugiat quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eum itaque nam necessitatibus, reiciendis quisquam!</p>
+        <h1 className={classes.teamHeader}>Meet The Team</h1>
+        <p className={classes.teamInfo}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, dicta
+          est consequuntur hic atque ullam non quis nisi fugiat quos. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Explicabo eum
+          itaque nam necessitatibus, reiciendis quisquam!
+        </p>
       </div>
     </div>
   );
