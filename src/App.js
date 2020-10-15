59ca0218d9cc1aa7f94b1e8ac72cc4728e8d6afe
Anuj Negi <anuj5negi5@gmail.com>
2020-10-15 18:06:04 +0530

    Loader setup complete

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
+import { FaWindows } from "react-icons/fa";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Layout />
-      </div>
-    </BrowserRouter>
-  );
-};
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    const el = document.querySelector(".loader-container");
+    if (el) {
+      el.remove();
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return null;
+    }
+    return (
+      <BrowserRouter>
+        <div>
+          <Layout />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
